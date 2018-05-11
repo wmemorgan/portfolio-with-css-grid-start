8bf9b6a93c15856356771415076c048ea43b7976
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-05-11 10:57:55 +0800

    Fix navigation slide out bugs

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,16 @@
 const navToggle = document.querySelector('.menu-button')
+const nav = document.querySelector('nav')
+const containerAll = document.querySelector('.container-all')
+
+const containerAllStyle = containerAll.style
+const bodyClassList = document.body.classList
 
 navToggle.addEventListener('click', () => {
-  document.body.classList.toggle('nav-is-open')
+  containerAllStyle.transition = 'transform 300ms ease-out'
+  bodyClassList.toggle('nav-is-open')
+})
+
+nav.addEventListener('click', () => {
+  containerAllStyle.transition = '0ms'
+  bodyClassList.remove('nav-is-open')
 })
