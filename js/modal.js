d695d9fc0b4d906f7fdf5fc08a8a62cc6fc7799f
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-05-11 14:16:09 +0800

    Add additional format and styling to modals

--- js/modal.js
+++ js/modal.js
@@ -11,9 +11,32 @@ portfolioContainer.addEventListener('click', e => {
   const modal = modalToggle.parentNode.nextElementSibling
   const closeButton = modal.querySelector('.modal-close')
   
-  modal.classList.add('is-open')
+  const modalOpen = () => {
+    modal.classList.add('is-open')
+    modal.style.animation = 'modalIn 500ms forwards'
+    document.body.style.overflowY = 'hidden'
+  }
+
+  const modalClose = () => {
+    modal.classList.remove('is-open')
+    modal.removeEventListener('animationend', modalClose)
+    
+  }
 
   closeButton.addEventListener('click', () => {
-    modal.classList.remove('is-open')
+    modal.style.animation = 'modalOut 500ms forwards'
+    modal.addEventListener('animationend', modalClose)
+    document.body.style.overflowY = 'scroll'
   })
+
+  document.addEventListener('keydown', e => {
+    if (e.keyCode === 27) {
+      modal.style.animation = 'modalOut 500ms forwards'
+      modal.addEventListener('animationend', modalClose)
+      document.body.style.overflowY = 'scroll'
+    }
+  })
+
+  modalOpen()
+
 })
